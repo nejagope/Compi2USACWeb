0b898ba4848c417fb364e106bb3b0b10331c78df
Nelson González <njgp86@gmail.com>
2018-03-01 18:31:23 -0600

    Se inicia con la creación del explorador.

--- Compi2USACWeb/src/compi2usacweb/Explorador.java
+++ Compi2USACWeb/src/compi2usacweb/Explorador.java
@@ -8,15 +8,21 @@ package compi2usacweb;
 import compilacion.Compilador;
 import compilacion.MotorExplorador;
 import java.awt.Color;
-import java.awt.FlowLayout;
+import java.awt.Dimension;
+import java.io.File;
+import javafx.scene.layout.Border;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.border.BevelBorder;
 
 /**
  *
  * @author Njgonzalez
  */
 public class Explorador extends javax.swing.JFrame {
-
+    
     public MotorExplorador motor;
     /**
      * Creates new form Explorador
@@ -34,18 +40,45 @@ public class Explorador extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        txtUrl = new javax.swing.JTextField();
+        btnRefresh = new javax.swing.JButton();
         tabsContainer = new javax.swing.JTabbedPane();
-        jButton2 = new javax.swing.JButton();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("USAC-Web");
         setSize(new java.awt.Dimension(1700, 1200));
 
-        jButton2.setText("Refresh");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        txtUrl.setFont(new java.awt.Font("Verdana", 0, 12)); // NOI18N
+        txtUrl.setText("C:/Users/Njgonzalez.AZUCAR/Documents/NetBeansProjects/Compi2USACWeb/Compi2USACWeb/src/compilacion/entrada1.chtml");
+        txtUrl.setToolTipText("");
+        txtUrl.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        txtUrl.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                txtUrlActionPerformed(evt);
             }
         });
+
+        btnRefresh.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/refresh.png"))); // NOI18N
+        btnRefresh.setBorder(null);
+        btnRefresh.setMaximumSize(new java.awt.Dimension(48, 48));
+        btnRefresh.setMinimumSize(new java.awt.Dimension(32, 32));
+        btnRefresh.setPreferredSize(new java.awt.Dimension(48, 48));
+        btnRefresh.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRefreshActionPerformed(evt);
+            }
+        });
+
+        jMenu1.setText("File");
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Edit");
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -54,39 +87,81 @@ public class Explorador extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(tabsContainer)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton2)
-                        .addGap(0, 809, Short.MAX_VALUE)))
-                .addContainerGap())
+                        .addComponent(tabsContainer)
+                        .addContainerGap())
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(txtUrl, javax.swing.GroupLayout.PREFERRED_SIZE, 975, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnRefresh, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 14, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(57, Short.MAX_VALUE)
-                .addComponent(jButton2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(tabsContainer, javax.swing.GroupLayout.PREFERRED_SIZE, 603, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(btnRefresh, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addComponent(txtUrl, javax.swing.GroupLayout.DEFAULT_SIZE, 32, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(tabsContainer, javax.swing.GroupLayout.DEFAULT_SIZE, 607, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        JPanel tab = new JPanel(new FlowLayout());
+    private void btnRefreshActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRefreshActionPerformed
+        refrescar();
+    }//GEN-LAST:event_btnRefreshActionPerformed
+
+    private void txtUrlActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtUrlActionPerformed
+        refrescar();
+    }//GEN-LAST:event_txtUrlActionPerformed
+
+    private void refrescar(){
+
+        String rutaArchivo = txtUrl.getText();
+        File file = new File(rutaArchivo);
+        String nombreArchivo = file.getName();
+        
         Compilador c = new Compilador();
-        String rutaArchivo = "C:\\Users\\Njgonzalez.AZUCAR\\Documents\\NetBeansProjects\\Compi2USACWeb\\Compi2USACWeb\\src\\compilacion\\entrada1.chtml";        
         c.compilar(rutaArchivo);
         c.mostrarTablaSimbolosConsola();
-        c.mostrarErroresConsola();        
-        MotorExplorador motor = new MotorExplorador(tab, c.tablaSimbolos);
-        motor.iniciar();
-        tab.setBackground(Color.WHITE);
-        tabsContainer.addTab("entrada1.chtl", tab);
+        c.mostrarErroresConsola();   
         
-    }//GEN-LAST:event_jButton2ActionPerformed
-
+        JPanel contenedor = new JPanel(null);
+        //contenedor.setPreferredSize(new Dimension(20000, 20000));
+        System.out.println(contenedor.getSize().toString());
+        contenedor.setBackground(Color.WHITE);
+        motor = new MotorExplorador(contenedor, c.tablaSimbolos);
+        motor.iniciar();        
+        
+        String tabTitle = nombreArchivo;
+        if (motor.tituloPestaña != null);
+            tabTitle = motor.tituloPestaña;
+        JScrollPane tab = new JScrollPane(contenedor ,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);        
+        
+        boolean addNewTab = true;
+        int cantTabs = tabsContainer.getTabCount();
+        int indiceTab = cantTabs;
+        for (int i= 0; i< cantTabs; i++){
+            if (tabTitle.equals(tabsContainer.getTitleAt(i))){                
+                addNewTab = false;
+                indiceTab = i;
+            }
+        }        
+        if (addNewTab)
+            tabsContainer.addTab(tabTitle, tab);
+        else{
+            tabsContainer.remove(indiceTab);
+            tabsContainer.insertTab(tabTitle, null, tab, rutaArchivo, indiceTab);
+        }
+        
+        
+        
+    }
+    
     private void compilar(){
         motor.iniciar();
         pack();
@@ -128,7 +203,11 @@ public class Explorador extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton btnRefresh;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JTabbedPane tabsContainer;
+    private javax.swing.JTextField txtUrl;
     // End of variables declaration//GEN-END:variables
 }
--- Compi2USACWeb/src/compilacion/MotorExplorador.java
+++ Compi2USACWeb/src/compilacion/MotorExplorador.java
@@ -8,8 +8,9 @@ package compilacion;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
-import javax.swing.JFrame;
+import java.awt.Dimension;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  *
@@ -17,44 +18,116 @@ import javax.swing.JPanel;
  */
 public class MotorExplorador {
     Container documento;
-    TablaSimbolos ts;
-    int x = -100;
-    int y = -100;
+    TablaSimbolos ts;    
+    public String tituloPestaña;
     
-    public MotorExplorador(Container documento, TablaSimbolos ts){
+    public MotorExplorador(Container documento, TablaSimbolos ts){        
         this.documento = documento;
         this.ts = ts;        
+        tituloPestaña = null;
     }
     
     public void iniciar(){
+        tituloPestaña = null;
         NodoAST nodoCHTML = ts.getComponenteByID("chtml").nodo;        
         agregarComponentes(documento, nodoCHTML);        
+    }
+    
+    private void agregarComponentes(Container container, NodoAST nodo){
+        int x=0, y=0;
+        int widthContainer = 0, heightContainer = 0;
+        for (NodoAST n: nodo.hijos){
+            Component nuevoComponent = getComponent(n);
+            if (nuevoComponent != null){
+                container.add(nuevoComponent);
+                nuevoComponent.setLocation(x, y);                
+                y += nuevoComponent.getPreferredSize().height;
+                heightContainer += y;
+                int anchoNuevoComponente = nuevoComponent.getPreferredSize().width;
+                if (anchoNuevoComponente > widthContainer)
+                    widthContainer = anchoNuevoComponente;
+                
+                if (nuevoComponent instanceof Container){
+                    agregarComponentes((Container)nuevoComponent, n);                
+                    continue;
+                }
+            }
+            agregarComponentes(container, n);
+        }
+        if (heightContainer > container.getPreferredSize().height)
+            container.setPreferredSize(new Dimension(container.getPreferredSize().width, heightContainer));
+        if (widthContainer > container.getPreferredSize().width)
+            container.setPreferredSize(new Dimension(widthContainer, container.getPreferredSize().height));
     }
     
     private Component getComponent(NodoAST nodo){
         switch(nodo.tipo){
             case panel:
-                JPanel panel = new JPanel();
-                //panel.setSize(200, 100);
-                //panel.setBackground(Color.BLACK);
-                //panel.setLocation(x, y);
-                x+= 100;
-                y += 100;
+                JPanel panel = new JPanel(null);                
+                panel.setSize(getDimension(nodo));
+                panel.setPreferredSize(getDimension(nodo));
+                panel.setBackground(getBackgroud(nodo));                
                 return panel;
-        }
+            case titulo:
+                try{                    
+                    NodoAST nodoTitTex = nodo.getHijo(TipoNodo.textoPlano);
+                    if (nodoTitTex != null)
+                        this.tituloPestaña = nodoTitTex.lexema;
+                }catch(Exception ex){}
+                break;
+            case texto:
+                    NodoAST nodoTex = nodo.getHijo(TipoNodo.textoPlano);
+                    if (nodoTex != null){
+                        JTextArea ta = new JTextArea(nodoTex.lexema);
+                        ta.setEditable(false);
+                        ta.setSize(getDimension(nodo));
+                        ta.setPreferredSize(getDimension(nodo));
+                        return ta;
+                    }
+                break;
+        }        
         return null;
     }
     
-    private void agregarComponentes(Container container, NodoAST nodo){
-        for (NodoAST n: nodo.hijos){
-            Component nuevoComponent = getComponent(n);
-            if (nuevoComponent != null){
-                container.add(nuevoComponent);
-                if (nuevoComponent instanceof Container)
-                    agregarComponentes((Container)nuevoComponent, n);                
+    private Dimension getDimension(NodoAST nodo){
+        int ancho = 500;
+        int alto = 20;
+        NodoAST atribs = nodo.getHijo(TipoNodo.atribs);
+        if (atribs != null){
+            NodoAST nodoAlto = atribs.getHijo(TipoNodo.alto);
+            if (nodoAlto != null){
+                String valStr = nodoAlto.getHijo(TipoNodo.cadenaValor).lexema;
+                try{
+                    alto = (int)(Double.parseDouble(valStr));
+                }catch(Exception ex){}
             }
-            agregarComponentes(container, n);
+            NodoAST nodoAncho = atribs.getHijo(TipoNodo.ancho);
+            if (nodoAlto != null){
+                String valStr = nodoAncho.getHijo(TipoNodo.cadenaValor).lexema;
+                try{
+                    ancho = (int)(Double.parseDouble(valStr));
+                }catch(Exception ex){}
+            }
         }
+        return new Dimension(ancho, alto);
     }
+    
+    private Color getBackgroud(NodoAST nodo){
+        Color color = Color.WHITE;
+        NodoAST atribs = nodo.getHijo(TipoNodo.atribs);
+        if (atribs != null){
+            NodoAST nodoFondo = atribs.getHijo(TipoNodo.fondo);
+            if (nodoFondo != null){
+                String valStr = nodoFondo.getHijo(TipoNodo.cadenaValor).lexema;
+                try{
+                    java.lang.reflect.Field field = Color.class.getField(valStr);
+                    color = (Color)field.get(null);
+                }catch(Exception ex){}
+            }
+        }
+        return color;
+    }
+    
+    
     
 }
